a21ecf1cade0ae1b93bd4b45d575c58c77a9e7a4
Muhammad Zain <mian.zain.dev@gmail.com>
2026-03-23 18:35:17 +0500

    GitHub Issue Resolves.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "map.h"
 #include "enemy.h"
 #include "saveload.h"
+#include "combat.h"
 
 enum class GameState { Running, PlayerDead, PlayerQuit };
 
@@ -87,42 +88,12 @@ int main()
                 break;
         }
 
-        // Check for enemy at new position
+        // Combat and movement
         bool combatOccurred = false;
-        for (int i = 0; i < MAX_ENEMIES; ++i)
-        {
-            if (enemies[i].alive &&
-                enemies[i].pos.row == newPos.row &&
-                enemies[i].pos.col == newPos.col)
-            {
-                player.target = &enemies[i];
-                combatOccurred = true;
-                break;
-            }
-        }
+        resolveCombat(player, enemies, MAX_ENEMIES, newPos, combatOccurred);
 
-        // Resolve combat if target exists
-        if (hasTarget(player))
-        {
-            std::cout << "Attacking " << player.target->name << "!\n";
-            damageEnemy(*player.target, 25);
-
-            if (!isEnemyAlive(*player.target))
-            {
-                std::cout << player.target->name
-                          << " defeated! +10 gold\n\n";
-                player.gold += 10;
-                clearTarget(player);
-            }
-            else
-            {
-                takeDamage(player, player.target->attackDamage);
-                std::cout << player.target->name << " retaliates for "
-                          << player.target->attackDamage << " damage!\n\n";
-            }
-        }
-        else if (!combatOccurred &&
-                 isWalkable(currentRoom, newPos.row, newPos.col))
+        if (!combatOccurred &&
+            isWalkable(currentRoom, newPos.row, newPos.col))
         {
             player.pos = newPos;
 
